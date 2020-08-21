db75b064f2c572d3cdc7af70e6dcae950231b966
ai382017 <62708290+ai382017@users.noreply.github.com>
2020-08-21 14:18:23 +0200

    Dodat panel sa dugmicima, logovanje korisnika

---
+++ 2/Projekat/src/apoteka/Main.java
@@ -0,0 +1,20 @@
+package apoteka;
+
+import java.awt.Insets;
+
+import javax.swing.JFrame;
+
+import apoteka.stranice.GlavniProzor;
+
+public class Main {
+
+	public static void main(String[] args) {
+		GlavniProzor p = GlavniProzor.getInstanca();
+		p.setVisible(true);
+		p.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Insets insets = p.getInsets();
+		p.setSize(850 + insets.left + insets.right, 620 + insets.top + insets.bottom);
+
+	}
+
+}
